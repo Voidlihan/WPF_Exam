c1e5025c0ddfa42c4289232558db9d482515b02a
e.alihangt@gmail.com <e.alihangt@gmail.com>
2019-12-02 21:38:49 +0600

    Add project files.

---
+++ WPF_Exam/Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace WPF_Exam
+{
+    public class Context : DbContext
+    {
+        public Context()
+        {
+            Database.EnsureCreated();            
+        }
+    }
+}
---
+++ WPF_Exam/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace WPF_Exam
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string urlSW = "https://swapi.co/api/people/";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadClick(object sender, RoutedEventArgs e)
+        {
+            if(String.IsNullOrWhiteSpace(page.Text))
+            {
+                MessageBox.Show("Страница не введена!");
+            }
+            else
+            {
+                int pnum = 0;
+                int.TryParse(page.Text, out pnum);
+                WebClient webClient = new WebClient();
+                string json = webClient.DownloadString(urlSW);
+                RootObject root = JsonConvert.DeserializeObject<RootObject>(json);
+                card.Items.Add(root);
+            }
+        }
+    }
+}
